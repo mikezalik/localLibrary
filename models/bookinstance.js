3183f4989f74c857e00a7802a70d05e1887d8ab1
mikezalik <mike.zalik@gmail.com>
2019-02-06 16:59:36 -0800

    date formatting

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
+
 
 var BookInstanceSchema = new Schema(
   {
@@ -16,6 +18,12 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
